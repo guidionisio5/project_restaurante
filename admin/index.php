2e32bad69471d4969820be450453b4b0de2fc388
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-08-29 22:26:31 -0300

    sessao finalizado

---
+++ admin/index.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="Cesar Szpak">
+    <link rel="icon" href="imagens/favicon.ico">
+
+    <title>Bistrô - Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/signin.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" method="POST" action="valida.php">
+        <h2 class="form-signin-heading">Área Restrita</h2>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+        <label for="inputPassword" class="sr-only">Senha</label>
+        <input type="password" name="senha" id="inputPassword" class="form-control" placeholder="Senha" required>
+        <button class="btn btn-lg btn-danger btn-block" type="submit">Acessar</button>
+      </form>
+	  <p class="text-center text-danger">
+		
+		</p>
+		<p class="text-center text-success">
+			
+		</p>
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ admin/valida.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    
+    include_once("../includes/conexao.php");
+ 
+    if(isset($_POST['email']) && isset($_POST['senha'])){
+        $usuario = mysqli_real_escape_string($conexao, $_POST['email']);
+         
+        $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+
+
+
+        $result_usuario = "SELECT * FROM tb_usuarios WHERE email = '$usuario' && senha = '$senha' LIMIT 1";
+        $result_usuario = mysqli_query($conexao, $result_usuario);
+        $resultado = mysqli_fetch_assoc($result_usuario);
+
+
+
+        if(isset($resultado)){
+            $_SESSION['usuarioId'] = $resultado['id'];
+            $_SESSION['usuarioNome'] = $resultado['nome'];
+            $_SESSION['usuarioNiveisAcessoId'] = $resultado['niveis_acesso_id'];
+            $_SESSION['usuarioEmail'] = $resultado['email'];
+            if($_SESSION['usuarioNiveisAcessoId'] == "1"){
+                header("Location: administrativo.php");
+            }elseif($_SESSION['usuarioNiveisAcessoId'] == "2"){
+                header("Location: colaborador.php");
+            }else{
+                header("Location: cliente.php");
+            }
+
+
+
+        }else{
+            
+            $_SESSION['loginErro'] = "Usuário ou senha inválido";
+            header("Location: index.php");
+        }
+
+    }else{
+            $_SESSION['loginErro'] = "Usuário ou senha inválido";
+            header("Location: index.php");
+    }
+?>
